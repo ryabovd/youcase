2eff94a897aa909eec6cef1360f3da3a33aecf53
ryabovd <ryabovd@outlook.com>
2024-12-02 14:49:17 +0700

    Remove debuggin lines.

--- case_common.py
+++ case_common.py
@@ -4,13 +4,16 @@ from bs4 import BeautifulSoup
 import requests
 #import datetime
 #import json
-#import csv
-#from send_email import send_notification
 
 
 
 url = 'https://abakansky--hak.sudrf.ru/modules.php?name=sud_delo&srv_num=2&name_op=case&case_id=75838512&case_uid=d9fcfb59-8b80-493a-af85-e354b361a998&delo_id=1540005'
 #url = 'https://abakansky--hak.sudrf.ru/modules.php?name=sud_delo&srv_num=2&name_op=case&case_id=75838512&case_uid=d9fcfb59-8b80-493a-af85-e354b361a998&delo_id=1540005'
+"""
+https://abakansky--hak.sudrf.ru/modules.php?name=sud_delo&srv_num=2&name_op=case&case_id=75838512&case_uid=d9fcfb59-8b80-493a-af85-e354b361a998&delo_id=1540005#
+https://abakansky--hak.sudrf.ru/modules.php?name=sud_delo&srv_num=2&name_op=case&case_id=75838512&case_uid=d9fcfb59-8b80-493a-af85-e354b361a998&delo_id=1540005#
+"""
+
 
 def getContent(url):
     session = get_session()
@@ -30,27 +33,17 @@ def getContent(url):
         'sec-fetch-site': 'none',
         'sec-fetch-user': '?1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        # 'Remote Address': '84.42.111.139:443',
-        # 'Referrer Policy': 'strict-origin-when-cross-origin'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
     }
-    data = session.get(url, headers=headers)
-    #print(data.text)
-    text = data.text
-    #print(type(data))
-    #print(BeautifulSoup(text, 'html.parser'))
-    #print(data.status_code)
-    print('/n', data.status_code)
-    if data.status_code == 200:
-        print('/n YES 200!!!!!')
+
     try:
-        html = data
+        data = session.get(url, headers=headers)
+        text = data.text
     except HTTPError as e:
         return None
     try:
         bs = BeautifulSoup(text, 'html.parser')
         content = bs.find(id = 'content')
-        #print(content)
     except AttributeError as e:
         return None
     return content
